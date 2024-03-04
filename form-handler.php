fa3f65b2d7551b42b1edc2e01149ba2084577f0f
pratyush <pratyushsharma200526@gmail.com>
2024-03-04 18:51:05 +0530

    fix: form submission

--- form-handler.php
+++ form-handler.php
@@ -1,25 +1,28 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-$email_from = 'webweave875@gmail.com';
+    $email_from = 'webweave875@gmail.com';
+    $email_subject = 'New Form Submission';
 
-$email_subject = 'New Form Submission';
+    $email_body = "User Name: $name\n".
+                   "User Email: $visitor_email\n".
+                   "Subject: $subject\n".
+                   "User Message: $message\n";
 
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                "Suject: $subject.\n".
-                "User Message: $message".\n";
+    $to = 'pratyushsharma200526@gmail.com';
 
-$to = 'pratyushsharma200526@gmail.com';
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
 
-$headers = "From: $email_from \r\n";
+    mail($to, $email_subject, $email_body, $headers);
 
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: ./contact.html");
+    header("Location: ./contact.html");
+} else {
+    // Redirect to the form if accessed directly without POST request
+    header("Location: ./contact.html");
+}
 ?>
